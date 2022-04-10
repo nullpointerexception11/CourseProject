ecea2e245df47093d6f59b7518f47cda5715910a
lostcode <lostcoding11@gmail.com>
2022-04-10 12:21:18 +0300

    js updated v2

---
+++ 04-JAVASCRIPT/assets/js/53-error-handling.js
@@ -0,0 +1,26 @@
+document.querySelector("#frmLogin").addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    // try{
+    //     getData();
+    // }catch(error){
+    //     alert("Bir hata olustu")
+    // }
+
+    try {
+        let email = document.querySelector('#email').value;
+        let password = document.querySelector("#password").value;
+
+
+        if(!isEmail(email)) throw("Geçerli bir eposta giriniz"); 
+        if(!password) throw("Şifre boş geçilemez"); 
+        document.querySelector("#frmLogin").submit();
+    } catch (error) {
+       alert(error);
+    }
+});
+function isEmail(email){
+    let reg = /\S+@\S+\.\S{2,5}$/;
+    return reg.test(email);
+}
